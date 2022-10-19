420b56026857ba7009c957b47dd5d8453551c5f4
jeffersonkeh <jeffersonkeh@gmail.com>
2022-10-19 11:19:43 +0800

    Add separate alert page

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, flash, redirect, url_for
 import ocr1
 import time
 import cv2
 import numpy as np
 
 app = Flask(__name__)
+app.secret_key = '12345'
+
+hot_car_list = ["ABCD1234"]
 
 # note check "ls /dev/video" to get the list of webcams
 camera = cv2.VideoCapture("/dev/video0")  # "/dev/video0 for laptop" "/dev/video4 for external"
@@ -12,13 +15,21 @@ camera = cv2.VideoCapture("/dev/video0")  # "/dev/video0 for laptop" "/dev/video
 # demo = ocr1.LiveDemo(src="/dev/video2").start()
 # time.sleep(1)
 
+count=0
+cropped_frame=np.zeros((720,1280,3), np.uint8)
+current_plate_num="ABCD1234"
 
 # def startDemo():
+#     global current_plate_num
 #     while demo.running():
 #         frame = demo.read()
         
 #         if len(demo.plateChars) > 0:
 #             print(demo.plateChars) 
+#             current_plate_num=demo.plateChars
+
+#             if current_plate_num in hot_car_list:
+#                 redirect(url_for('alert'))
 
 #         ret, buffer = cv2.imencode('.jpg', frame)
 #         frame = buffer.tobytes()
@@ -34,9 +45,6 @@ camera = cv2.VideoCapture("/dev/video0")  # "/dev/video0 for laptop" "/dev/video
 
 #     demo.stop()
 
-count=0
-cropped_frame=np.zeros((720,1280,3), np.uint8)
-current_plate_num="ABCD1234"
 def gen_frames(camera=camera):  # generate frame by frame from camera
     global count
     global cropped_frame
@@ -62,6 +70,10 @@ def get_cropped_frame():
     yield (b'--frame\r\n'
         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
+@app.route('/alert')
+def alert():
+    return render_template('alert.html')
+
 @app.route('/cropped_feed')
 def cropped_feed():
     #Video streaming route. Put this in the src attribute of an img tag
@@ -72,10 +84,10 @@ def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/video_feed2')
-def video_feed2():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(camera2), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed2')
+# def video_feed2():
+#     #Video streaming route. Put this in the src attribute of an img tag
+#     return Response(gen_frames(camera2), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def index():
